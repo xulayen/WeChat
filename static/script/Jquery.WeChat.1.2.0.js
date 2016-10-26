2a3c4c5c4c6d1fa5aee0a186a13e025831024fb8
xulayen <l>
2016-10-26 18:44:50 +0800

    try

--- static/script/Jquery.WeChat.1.2.0.js
+++ static/script/Jquery.WeChat.1.2.0.js
@@ -678,17 +678,17 @@
     $.fn.GetLocation = $.GetLocation = function (success) {
         var _self = this;
         try {
-            WX.ready(function () {
-                WX.getLocation({
-                    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-                    success: function (res) {
-                        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                        var speed = res.speed; // 速度，以米/每秒计
-                        var accuracy = res.accuracy; // 位置精度
-                        success && success.call(_self, res, latitude, longitude, speed, accuracy);
-                    }
-                });
+            WX.getLocation({
+                success: function (res) {
+                    var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                    var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                    var speed = res.speed; // 速度，以米/每秒计
+                    var accuracy = res.accuracy; // 位置精度
+                    success && success.call(_self, res, latitude, longitude, speed, accuracy);
+                },
+                cancel: function (res) {
+                    alert('用户拒绝授权获取地理位置');
+                }
             });
         } catch (e) {
             d.error('GetLocation error' + e.message);
@@ -761,7 +761,7 @@
      * 创建卡券
      * @type {Function}
      */
-    $.fn.CreateCard = $.CreateCard = function (data, openid, code, success) {
+    $.fn.CreateCard = $.CreateCard = function (data, success) {
         var _self = this;
         try {
             $.ajax({
